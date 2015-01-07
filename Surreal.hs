195b061a678dd514859cd7588bed35898d9ac30a
Ben Elliott <benjam.elliott@gmail.com>
2015-01-06 19:51:12 -0700

    More additions + license
    Added open source MIT license, some more added features to the surreal
    code.

--- Surreal.hs
+++ Surreal.hs
@@ -1,8 +1,10 @@
 import qualified Data.Set as S
+import Data.Maybe
+import Data.List
 
 data Surreal = Surreal { left  :: S.Set Surreal
                        , right :: S.Set Surreal
-                       } deriving (Show)
+                       }
 
 surreal :: [Surreal] -> [Surreal] -> Surreal
 l `surreal` r = (S.fromList l) `Surreal` (S.fromList r)
@@ -14,6 +16,29 @@ sNull = S.empty
 sZero = sNull ~|~ sNull
 sOne = (S.singleton sZero) ~|~ sNull
 
+slmax, srmin :: Surreal -> Maybe Surreal
+slmax srl = if not (S.null (left srl)) then Just (S.findMax (left srl)) else Nothing
+srmin srl = if not (S.null (right srl)) then Just (S.findMin (right srl)) else Nothing
+
+cleanSurreal :: Surreal -> Surreal
+cleanSurreal s
+	| (left s)==sNull && (right s)==sNull 	= sZero
+	| (left s)==sNull 	= Surreal sNull ((S.singleton . cleanSurreal . fromJust . srmin) s)
+	| (right s)==sNull	= Surreal ((S.singleton . cleanSurreal . fromJust . slmax) s) sNull
+	| ((fromJust . slmax) s)<sZero && ((fromJust . srmin) s)>sZero		= sZero
+	| otherwise	= Surreal ((S.singleton . cleanSurreal . fromJust . slmax) s) ((S.singleton . cleanSurreal . fromJust . srmin) s)
+
+numeric :: Surreal -> Bool
+numeric s = (S.intersection (left s) (right s) == S.empty)
+    && (\lmax rmin -> if lmax == Nothing then True else (if rmin == Nothing then True else lmax < rmin)) (slmax s) (srmin s)
+	
+instance Show Surreal where
+	show s
+		| (left s)==sNull && (right s)==sNull 	= "{|}"
+		| (left s)==sNull 	= "{|"++((intercalate ",". S.toList . S.map show)(right s))++"}"
+		| (right s)==sNull	= "{"++((intercalate ",". S.toList . S.map show) (left s))++"|}"
+		| otherwise	= "{"++((intercalate ",". S.toList . S.map show) (left s))++"|"++((intercalate ",". S.toList . S.map show)(right s))++"}"
+
 instance Ord Surreal where
     x <= y
         | S.null (left x) && S.null (right x) && S.null (left y) && S.null (right y) = True
@@ -22,14 +47,6 @@ instance Ord Surreal where
 
 instance Eq Surreal where
     x == y = (x <= y) && (y <= x)
-
-slmax, srmin :: Surreal -> Maybe Surreal
-slmax srl = if not (S.null (left srl)) then Just (S.findMax (left srl)) else Nothing
-srmin srl = if not (S.null (right srl)) then Just (S.findMin (right srl)) else Nothing
-
-numeric :: Surreal -> Bool
-numeric s = (S.intersection (left s) (right s) == S.empty)
-    && (\lmax rmin -> if lmax == Nothing then True else (if rmin == Nothing then True else lmax < rmin)) (slmax s) (srmin s)
 
 -- Only numeric surreals will be used entirely correctly here, though the
 -- effects of numeric functions may have useful effects on non-numeric surreals
@@ -58,7 +75,6 @@ instance Num Surreal where
 			l2 = ((multnumset sy (right sx)) `addsets` ((multnumset sx (right sy)) `subtractsets` (multsets (right sx) (right sy))))
 			r1 = ((multnumset sy (left sx)) `addsets` ((multnumset sx (right sy)) `subtractsets` (multsets (left sx) (right sy))))
 			r2 = ((multnumset sy (right sx)) `addsets` ((multnumset sx (left sy)) `subtractsets` (multsets (right sx) (left sy))))
-			
 	fromInteger i
 		| i < 0 	= (-sOne) + fromInteger (i+1)
 		| i == 0	= sZero
@@ -70,3 +86,41 @@ instance Num Surreal where
 		| s < sZero		= -sOne
 		| s == sZero	= sZero
 		| s > sZero		= sOne
+
+instance Real Surreal where
+	toRational s = undefined
+
+instance Fractional Surreal where
+	fromRational s 	= undefined
+	(/) sn sd 		= undefined
+
+instance Floating Surreal where
+	pi 		= undefined
+	exp s 	= undefined
+	log s 	= undefined
+	sin s 	= undefined
+	cos s 	= undefined
+	asin s 	= undefined
+	atan s 	= undefined
+	acos s 	= undefined
+	sinh s 	= undefined
+	cosh s 	= undefined
+	asinh s = undefined
+	atanh s = undefined
+	acosh s = undefined
+
+instance RealFrac Surreal where
+	properFraction s = undefined
+
+instance RealFloat Surreal where
+	floatRadix s 		= undefined
+	floatDigits s 		= undefined
+	floatRange s 		= undefined
+	decodeFloat s 		= undefined
+	encodeFloat s 		= undefined
+	isNaN s 			= undefined
+	isInfinite s 		= undefined
+	isDenormalized s 	= undefined
+	isNegativeZero s 	= undefined
+	isIEEE s 			= undefined
+
